1adbd5745f523bb4a24ad3fe4a7b6487712c16ef
Mario Bonanno <lordmarius@hotmail.it>
2025-06-09 11:08:43 +0200

    Fixed memory share, optimized some methods

--- include/NameService.h
+++ include/NameService.h
@@ -3,16 +3,20 @@
 #include <Arduino.h>
 #include <Message.h>
 #include <map>
+#include <array>
 
 class NameService {
-  public:
-    std::map<String, const uint8_t *> address_table;
-    ~NameService();
+public:
+    static constexpr size_t ADDRESS_SIZE = 2; // As for LoraMesher address specs
 
-    void add(const String name, const uint8_t *addr);
-    bool exists(const String name);
-    const uint8_t *get(const String name);
-    void remove(const String name);
+    ~NameService() = default;
+
+    void add(const String& name, const uint8_t* addr);
+    bool exists(const String& name) const;
+    const uint8_t* get(const String& name) const;
+    void remove(const String& name);
+
+    std::map<String, std::array<uint8_t, ADDRESS_SIZE>> address_table;
 };
 
 #endif
--- src/NameService.cpp
+++ src/NameService.cpp
@@ -1,17 +1,25 @@
 #include <NameService.h>
 
-NameService::~NameService() { this->address_table.clear(); }
-
-void NameService::add(const String name, const uint8_t *addr) {
-    this->address_table[name] = addr;
+void NameService::add(const String& name, const uint8_t* addr) {
+    std::array<uint8_t, ADDRESS_SIZE> copy;
+    for (size_t i = 0; i < ADDRESS_SIZE; ++i) {
+        copy[i] = addr[i];
+    }
+    address_table[name] = copy;
 }
 
-bool NameService::exists(const String name) {
-    return this->address_table.count(name) > 0;
+bool NameService::exists(const String& name) const {
+    return address_table.count(name) > 0;
 }
 
-const uint8_t *NameService::get(const String name) {
-    return this->address_table[name];
+const uint8_t* NameService::get(const String& name) const {
+    auto it = address_table.find(name);
+    if (it != address_table.end()) {
+        return it->second.data();
+    }
+    return nullptr;
 }
 
-void NameService::remove(const String name) { this->address_table.erase(name); }
+void NameService::remove(const String& name) {
+    address_table.erase(name);
+}
